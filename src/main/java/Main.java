d8b68a02420568e6b101fc70e1175889fe94a969
Ekaterina Martynova <ekaterina.martynova89@mail.ru>
2022-09-19 11:14:43 +0300

    Домашняя работа

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("110001", "1011011"));
+        System.out.println(binOps.mult("110001", "1011011"));
+    }
+}
